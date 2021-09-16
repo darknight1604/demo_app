cca80fb638941bb8e38558165f75214b79c61173
Quoc Tan <darknight1604@gmail.com>
2021-09-16 16:40:42 +0700

    Darkmode

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:demo_app/repositories/user_repository.dart';
 import 'package:demo_app/route/route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
@@ -26,6 +27,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // var brightness = SchedulerBinding.instance!.window.platformBrightness;
+    // bool isDarkMode = brightness == Brightness.dark;
+    // print(isDarkMode);
     return GestureDetector(
       onTap: () {
         FocusScopeNode currentFocus = FocusScope.of(context);
@@ -46,7 +50,12 @@ class MyApp extends StatelessWidget {
         child: MaterialApp(
           title: 'Interview Demo',
           theme: ThemeData(
-            backgroundColor: Colors.white,
+            brightness: Brightness.light,
+            /* light theme settings */
+          ),
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            /* dark theme settings */
           ),
           onGenerateRoute: RouteGenerator.generateRoute,
           initialRoute: '/',
